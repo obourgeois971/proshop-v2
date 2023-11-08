b18f6ffc4e2f09a90de1ce1e37862fb40b332bfb
Olivier <bourgeoisolivier@hotmail.fr>
2023-11-08 21:55:38 +0100

    log out and clear cookies

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+import cookieParser from "cookie-parser";
 
 const port = process.env.PORT || 5000;
 
@@ -15,6 +16,9 @@ const app = express();
 // Body parser middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// Cookie parser moddleware
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("API is running...");
