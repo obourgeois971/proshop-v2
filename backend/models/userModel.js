16fb43c0613ad43277859ca1303ad3166a09d8f5
Olivier <bourgeoisolivier@hotmail.fr>
2023-11-08 16:53:04 +0100

    auth User and jwt

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const userSchema = new mongoose.Schema(
   {
@@ -23,6 +24,11 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
+
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
 const User = mongoose.model("User", userSchema);
 
